35b15fbfe0dfa81fb627c6d0ff490a19ef66f4ec
demo1511 <cosmos.bvn@gmail.com>
2026-02-28 11:30:46 +0530

    updated base url

--- frontend/src/admin/api/adminApi.js
+++ frontend/src/admin/api/adminApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/admin", // adjust backend URL
+  baseURL: "https://mcq-test-b6ao.onrender.com/api/admin", // adjust backend URL
 });
 
 // Admin login
