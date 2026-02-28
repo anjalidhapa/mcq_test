f4f5866db6290c70aa2b52be94d78b7d528479a0
demo1511 <cosmos.bvn@gmail.com>
2026-02-28 11:50:57 +0530

    added cors in server 2

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,13 @@ app.get("/", (req, res) => {
 });
 
 // middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://mcq-test-psi.vercel.app/",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // routes
